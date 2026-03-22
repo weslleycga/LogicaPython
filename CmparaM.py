3436b8b0e49bab7ed7e770897bf55fa1cc98ed1a
Weslley Cutrim Gois Amaral <weslleycga@gmail.com>
2026-03-22 14:58:07 -0300

    Conversor de cm para metros

---
+++ CmparaM.py
@@ -0,0 +1,3 @@
+cm = float(input("Digite a medida em centímetros: ")) #recebe o valor do usuario em centimetros e converte para metros
+m = cm / 100
+print(f"{cm} centimetros equivalem a {m} metros.")
